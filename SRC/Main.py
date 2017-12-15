29c5166142603624a80bd9572e8e5c4b877112ab
Laurier Lavoie-Giasson <laurier.lg@gmail.com>
2017-12-14 19:56:01 -0500

    termine les parametres pour multithread

--- SRC/Main.py
+++ SRC/Main.py
@@ -12,6 +12,7 @@ from SQLConnector import SQLConnector
 from Calculator import *
 import time
 from numpy import single
+from test.regrtest import multiprocessing
 
 #ANALYSER LA LIGNE DE COMMANDE/INPUT DU L'USAGER ========================================================================
 def user_Input(DB):
@@ -37,6 +38,7 @@ def user_Input(DB):
             idxNbThreads = sys.argv.index(argument)
         elif argument == "-st":
             singleThread = True
+            NombreThreads=0
         elif "-" in argument:
             strangerDanger = True
             idxStranger = sys.argv.index(argument)
@@ -107,13 +109,22 @@ def user_Input(DB):
             return(False,)
         if singleThread and not multiThread:
             multiThread=False
+            NombreThreads=0
         if multiThread and not singleThread:
             try:
+                nbProcesseurs = multiprocessing.cpu_count()
                 NombreThreads = int(sys.argv[idxNbThreads+1])
+                if(NombreThreads >= nbProcesseurs):
+                    print("\n   /WARNING/  la configuration désirée serait inefficace, reconfiguration automatique.\n")
+                    print("   /PROGRAM/  Configuration automatique -- nombre de processeurs:",nbProcesseurs,"nombre de processus à utiliser:",NombreThreads)
             #En cas de valeur non-numerique
             except ValueError:
-                print("\n     /ERROR/  Paramètre invalide, ('"+sys.argv[idxNbThreads+1]+"') n'est pas une valeur numerique.") 
-                return (False,)
+                if sys.argv[idxNbThreads+1] == "auto":
+                    NombreThreads = nbProcesseurs//2
+                    print("   /PROGRAM/  Configuration automatique -- nombre de processeurs:",nbProcesseurs,"nombre de processus à utiliser:",NombreThreads)
+                else:
+                    print("\n     /ERROR/  Paramètre invalide, ('"+sys.argv[idxNbThreads+1]+"') n'est pas une valeur numerique.") 
+                    return (False,)
             except IndexError:
                 print("\n     /ERROR/  Paramètre manquant, -mt doit être suivi d'une valeur numérique (combien de processus exécuter? par défaut 1)")
                 NombreThreads = 1
@@ -122,11 +133,11 @@ def user_Input(DB):
             return(True,"mots",ValeurTaille,ListeMots,NombreDeMots,multiThread,NombreThreads)
         else:
             return(True,"nb",ValeurTaille,ValeurCen,NombreDeMots, multiThread,NombreThreads)
-        
+
     #Si parametre invalide    
-    else:  
+    else:
         print("\n     /ERROR/  Paramètre invalide, ('"+sys.argv[idxStranger]+"') n'est pas un argument valide.") 
-        return (False,)      
+        return (False,)
 
 #MAIN ===================================================================================================================
 def main():    
@@ -139,24 +150,27 @@ def main():
         TailleFenetre = rep[2]
         NombreDeMots = rep[4]
         multiThread = rep[5]
-        if multiThread:
-            NombreThreads = rep[6]
+        NombreThreads = rep[6]
+        if NombreThreads == 0:
+            NombreThreads = 1
         #Si entree par mots
         if rep[1] == "mots":
             ListeDesMots = rep[3]
-            print("   /PROGRAM/  [Clustering] "+" Taille de la fenetre :",TailleFenetre,"  Liste de mots :",ListeDesMots," Nb de mots à garder :",NombreDeMots)
+            print("   /PROGRAM/  [Clustering] "+" Taille de la fenetre :",TailleFenetre,"  Liste de mots :",ListeDesMots)
+            print("   /PROGRAM/  [Clustering] "+" Nb de mots à garder :", NombreDeMots,  " mode d'exécution: multiprocessing ->",multiThread,"nombre de processus:",NombreThreads)
             Params = (rep[1],TailleFenetre,ListeDesMots,NombreDeMots)
             
         #Si entree par nombre
         else:
             NombreCentroides = rep[3]
-            print("   /PROGRAM/  [Clustering] "+" Taille de la fenetre :",TailleFenetre,"  Nb de centroides :",NombreCentroides," Nb de mots à garder :",NombreDeMots)
+            print("   /PROGRAM/  [Clustering] "+" Taille de la fenetre :",TailleFenetre,"  Nb de centroides :",NombreCentroides)
+            print(" Nb de mots à garder :",NombreDeMots," mode d'exécution: multiprocessing ->",multiThread,"nombre de processus:",NombreThreads)
             Params = (rep[1],TailleFenetre,NombreCentroides,NombreDeMots)
         #initialisation de la calculatrice
         Calc = Calculator1(Params,Database)
         #multithread
         if multiThread:
-            startTime = operationsMultiThread(Calc, NombreThreads)
+            startTime, threads = operationsMultiThread(Calc, NombreThreads)
             print("temps pour toutes les itérations:", time.time()-startTime,"secondes")
         #singlethread
         else:
@@ -171,7 +185,7 @@ def operationsMultiThread(Calc, NombreThreads):
         print("itération",i,"en",time.time()-timeiter,"secondes.")
         i+=1
         timeiter=time.time()
-    return startTime
+    return startTime, threads
 #             
 #EXECUTION DU PROGRAMME =================================================================================================
 if __name__ == '__main__':
